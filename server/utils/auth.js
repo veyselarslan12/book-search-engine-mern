54245e4d47f5f7a285db1fdefc5b6aa26c00be27
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-07-31 23:22:14 -0500

    auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,3 +1,4 @@
+const { GraphQLError } = require('graphql')
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -5,6 +6,12 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  // Adding authentication error
+  AuthenticationError: new GraphQLError('Could not authenticate user', {
+    extensions: {
+      code: 'UNAUTHENTICATED'
+    }
+  }),
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
