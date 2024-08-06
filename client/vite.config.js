bbcd0bc06ffdc3cc560a317dc9bdac948e8120f9
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-08-05 22:28:03 -0500

    vite config changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3000,
+    open: true,
     proxy: {
       '/graphql': {
         target: 'http://localhost:3001',
